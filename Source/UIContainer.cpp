3d8cfc4325fe95dce867319f0011a0b686603d9c
Guilherme Cunha <Guilherme_Cunha@thecdm.ca>
2015-06-15 20:01:28 -0700

    removing unstable files from master trunk

--- Source/UIContainer.cpp
+++
@@ -1,9 +0,0 @@
-////
-////  UIContainer.cpp
-////  opengl_testing_mag
-////
-////  Created by Guilherme Cunha on 2015-01-12.
-////  Copyright (c) 2015 Guilherme Cunha. All rights reserved.
-////
-//
-//#include "UIContainer.hpp"
